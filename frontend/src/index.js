9b521c5783ffe42a3a3496af00c0b745634f3077
Priyanshu741741 <priyanshu741741@gmail.com>
2025-03-28 23:01:41 +0530

    pls2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,20 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+// Mock Firebase for apps that were using Firebase previously
+window.firebase = {
+  initializeApp: () => {},
+  auth: () => ({
+    onAuthStateChanged: (callback) => {
+      callback(null);
+      return () => {};
+    },
+    signInWithEmailAndPassword: () => Promise.resolve({}),
+    createUserWithEmailAndPassword: () => Promise.resolve({}),
+    signOut: () => Promise.resolve({})
+  })
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
